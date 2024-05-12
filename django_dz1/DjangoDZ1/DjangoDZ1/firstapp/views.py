560a23a0484819738b82f70a9962378d5c69b603
alexeyb7 <alexeyb7@gmail.com>
2024-05-12 23:33:39 +0300

    Modify DZ3

--- django_dz1/DjangoDZ1/DjangoDZ1/firstapp/views.py
+++ django_dz1/DjangoDZ1/DjangoDZ1/firstapp/views.py
@@ -6,4 +6,21 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello world')
+    index1 = ('<!DOCTYPE html><html lang="ru"><head>'
+            '<meta charset="UTF-8">'
+            '<title>Главная страница</title>'
+            '</head><body>Главная страница</body></html>')
+    return HttpResponse(index1)
+
+
+def about(request):
+    about1 = ('<!DOCTYPE html> <html lang="ru" > '
+              '<head> <meta charset="UTF-8"> <title>Сведения обо мне</title> '
+              '</head>'
+              ' <body> <header> <h1>Обо мне </h1> </header>'
+              ' <main><p>Многострочный текст</p>'
+              ' <ul> таки да</ul>'
+              ' <ul> таки нет</ul>'
+              ' </main> </body> </html>')
+    return HttpResponse(about1)
+
